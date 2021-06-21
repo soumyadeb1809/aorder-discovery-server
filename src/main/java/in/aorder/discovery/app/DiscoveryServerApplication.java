e7632f962feff9906df9abcc3b688986b1f0e3dd
Soumya Deb <soumyadeb1809@gmail.com>
2021-06-21 23:44:28 +0530

    Enable eureka server

--- src/main/java/in/aorder/discovery/app/DiscoveryServerApplication.java
+++ src/main/java/in/aorder/discovery/app/DiscoveryServerApplication.java
@@ -1,9 +1,11 @@
-package in.aorder.discovery;
+package in.aorder.discovery.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("in.aorder.discovery")
 public class DiscoveryServerApplication {
 
 	public static void main(String[] args) {
